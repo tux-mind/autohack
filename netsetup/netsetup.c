5091136a335cea4cf01ed0734ba44edf96b808e6
N3tAuG3r <andrea.columpsi@gmail.com>
2013-12-11 12:29:29 +0100

    Update netsetup.c

--- netsetup/netsetup.c
+++ netsetup/netsetup.c
@@ -1,13 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h> /* for strncpy */
+#include "netsetup.h"
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <arpa/inet.h>
 
 int main()
 {
