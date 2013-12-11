9fb55733cdeb2997d5bfc9857dd5ba7b9962814b
N3tAuG3r <andrea.columpsi@gmail.com>
2013-12-11 12:28:59 +0100

    Create netsetup.h

---
+++ netsetup/netsetup.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> /* for strncpy */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
